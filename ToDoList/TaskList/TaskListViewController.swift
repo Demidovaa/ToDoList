1fab4cd4f6565e4ff9eb13c49b74ea1bca5333f7
Anastasia <demidova.nastasya99@gmail.com>
2021-03-22 15:50:35 +0300

    Added remove task from list

--- ToDoList/TaskList/TaskListViewController.swift
+++ ToDoList/TaskList/TaskListViewController.swift
@@ -17,8 +17,7 @@ class TaskListViewController: UIViewController {
     @IBOutlet private weak var addButtonView: UIView!
     
     //MARK: - Properties
-    
-    private var countCell = 0
+
     private var textTask: [String] = []
     
     //MARK: - Lifecycle
@@ -66,9 +65,8 @@ class TaskListViewController: UIViewController {
         popup.closePopup = { [weak self] text in
             guard let self = self else { return }
             if !text.isEmpty {
-                self.countCell += 1
                 self.textTask.append(text)
-                self.tableView.insertRows(at: [IndexPath(row: self.countCell - 1, section: 0)], with: .automatic)
+                self.tableView.insertRows(at: [IndexPath(row: self.textTask.count - 1, section: 0)], with: .automatic)
             }
             self.dismiss(animated: true)
             UIView.animate(withDuration: 0.3) {
@@ -83,18 +81,46 @@ class TaskListViewController: UIViewController {
 
 //MARK: - Extension
 
-extension TaskListViewController: UITableViewDelegate {
-    
-}
-
-extension TaskListViewController: UITableViewDataSource {
+extension TaskListViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return countCell
+        return textTask.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell", for: indexPath) as? TaskTableViewCell else { return UITableViewCell() }
         cell.configureCell(text: textTask[indexPath.row])
         return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//        guard editingStyle == .delete else { return }
+//        textTask.remove(at: indexPath.row)
+//        tableView.deleteRows(at: [indexPath], with: .automatic)
+//    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+
+        var actions = [UIContextualAction]()
+
+        let delete = UIContextualAction(style: .normal, title: nil) { [weak self] (contextualAction, view, completion) in
+            guard let self = self else { return }
+            self.textTask.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .left)
+        
+            completion(true)
+        }
+        
+        let configureImage = UIImage.SymbolConfiguration(pointSize: 12.0, weight: .bold, scale: .large)
+        
+        delete.image = UIImage(systemName: "trash",
+                               withConfiguration: configureImage)?.withTintColor(.white, renderingMode: .alwaysTemplate).addBackgroundCircle(.systemRed)
+        delete.backgroundColor = .systemBackground // Update color
+        
+        actions.append(delete)
+
+        let config = UISwipeActionsConfiguration(actions: actions)
+        config.performsFirstActionWithFullSwipe = false
+
+        return config
     }
 }
